ed5d537f9702e39f3cad9ecbe933382d49a1ac9d
sarthak425 <sarthak.khatpe123@gmail.com>
2026-02-13 19:40:48 +0530

    Change badge text color to amber-900

--- components/Common/Badge.jsx
+++ components/Common/Badge.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Badge = ({ title }) => {
     return (
-        <span className='py-2 px-3 text-xs text-Snow bg-EveningBlack rounded-full'>{title}</span>
+        <span className='py-2 px-3 text-xs text-amber-900 bg-EveningBlack rounded-full'>{title}</span>
     )
 }
 
