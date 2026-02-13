f35157e807d34f288fa2aaf8ad957129f186980e
sarthak425 <sarthak.khatpe123@gmail.com>
2026-02-13 19:37:59 +0530

    Update portfolio.js

--- pages/api/portfolio.js
+++ pages/api/portfolio.js
@@ -1,52 +1,35 @@
 const portfolio = [
     {
         id: 0,
-        projectName: "JMM ERP",
-        url: "https://jmmerp.com/",
-        image: "projects/erp.png",
-        projectDetail: "Restructure your business with JMM ERP’s all-in-one cloud-based ERP. JMM ERP provides a robust, cost-effective online solution with state-of-the-art software to increase business value and enhance customer impact.",
+        projectName: "Lemon Soap",
+        image: "projects/lemon.png",
+        projectDetail: "Rich in Vitamin C, it works as a natural astringent to brighten the complexion and lighten minor blemishes. ",
         technologiesUsed: [
             {
-                tech: "ReactJS"
+                tech: "Natural brightening effect"
             },
             {
-                tech: ".net"
+                tech: "Reduces dark spots"
             },
             {
-                tech: "GraphQL"
+                tech: "Controls oil"
             },
             {
-                tech: "Ant Design"
+                tech: "Refreshes skin"
+            }, 
+             {
             },
             {
-                tech: "TailwindCSS"
+                tech: "Reduces pigmentation"
             },
         ]
     },
+    
     {
         id: 0,
-        projectName: "ABSCO",
-        url: "https://absco.sa/",
-        image: "projects/absco.png",
-        projectDetail: "ABSCO offer unparalleled business consulting solutions to meet client’s unique needs.",
-        technologiesUsed: [
-            {
-                tech: "ReactJS"
-            },
-            {
-                tech: "Ant Design"
-            },
-            {
-                tech: "TailwindCSS"
-            },
-        ]
-    },
-    {
-        id: 0,
-        projectName: "JMM Technologies",
-        url: "https://jmm.ltd/",
-        image: "projects/jmm.png",
-        projectDetail: "JMM Technologies is a start-up turned technology company with extensive years of experience delivering digital solutions. As a fast-growing tech firm, we help diverse businesses and organizations. Our expertise in innovative, state-of-the-art digital services accelerates business growth and enhances customer impact.",
+        projectName: "Rice Milk Soap",
+        image: "projects/rice milk.jpeg",
+        projectDetail: " Known for its softening and brightening effects, it nourishes the skin with vitamins and helps achieve a milky smooth texture. ",
         technologiesUsed: [
             {
                 tech: "ReactJS"
@@ -66,83 +49,30 @@ const portfolio = [
         ]
     },
     {
+                id: 2,
+                projectName: "Haldi Soap",
+                image: "projects/haldi.jpeg",
+                projectDetail: " Combines the brightening power of Turmeric (Haldi) with the gentle exfoliation of Gram Flour (Besan). It is excellent for tan removal, fading dark spots, and achieving a radiant glow.",
+                technologiesUsed: [
+                    {
+                        tech: "ReactJS"
+                    },
+                    {
+                        tech: "NodeJS"
+                    },
+                    {
+                        tech: "Ant Design"
+                    },
+                    {
+                        tech: "TailwindCSS"
+                    },
+                ]
+            },
+            {
         id: 0,
-        projectName: "ALSN",
-        url: "https://alsn.app/",
-        image: "projects/alsn.png",
-        projectDetail: "ASLN drives digital transformation with certified document translation services. In an interconnected world where seamless communication is imperative, they are strategically positioned to cater to the needs of businesses, individuals, and organizations.",
-        technologiesUsed: [
-            {
-                tech: "ReactJS"
-            },
-            {
-                tech: ".net"
-            },
-            {
-                tech: "REST API's"
-            },
-            {
-                tech: "Ant Design"
-            },
-            {
-                tech: "TailwindCSS"
-            },
-        ]
-    },
-    {
-        id: 0,
-        projectName: "FirmSanad",
-        url: "/domain-expired",
-        image: "projects/firmsanad.png",
-        projectDetail: "Invest in Saudi Arabia, Streamline Your Business Setup Process in Saudi Arabia with FirmSanad Simplify the process of obtaining licenses and starting a business in Saudi Arabia with our streamlined platform.",
-        technologiesUsed: [
-            {
-                tech: "ReactJS"
-            },
-            {
-                tech: ".net"
-            },
-            {
-                tech: "REST API's"
-            },
-            {
-                tech: "Ant Design"
-            },
-            {
-                tech: "TailwindCSS"
-            },
-        ]
-    },
-    {
-        id: 0,
-        projectName: "Fateh Al Mustaqbil",
-        url: "https://fatehtour.com/",
-        image: "projects/fateh.png",
-        projectDetail: "Fateh Al Mustaqbil is a one-of-a-kind travel agency designed to cater to the interests of travelers across the globe, founded in 2023 in Riyadh, Saudi Arabia. Their expertise lies in crafting exceptional journeys tailored to each client's unique requirements, whether it be ticketing, hotel bookings, or visa assistance.",
-        technologiesUsed: [
-            {
-                tech: "ReactJS"
-            },
-            {
-                tech: ".net"
-            },
-            {
-                tech: "REST API's"
-            },
-            {
-                tech: "Ant Design"
-            },
-            {
-                tech: "TailwindCSS"
-            },
-        ]
-    },
-    {
-        id: 0,
-        projectName: "Tojjar",
-        url: "/domain-expired",
-        image: "projects/tojjar.png",
-        projectDetail: "Tojjar is ecommerce website,  where you can open your own store and sell products of different brands!",
+        projectName: "Charcoal Soap",
+        image: "projects/charcoal.jpeg",
+        projectDetail: " Known for its detoxifying ability, it acts like a magnet to draw out deep-seated dirt, pollution, and oil from pores. Brightening & Exfoliating Soaps ",
         technologiesUsed: [
             {
                 tech: "Nextjs with SSR"
@@ -162,11 +92,80 @@ const portfolio = [
         ]
     },
     {
+        id: 0,
+        projectName: "Uthane Soap",
+        image: "projects/uthane.jpeg",
+        projectDetail: "Soap: A traditional Ayurvedic blend of herbs and pulses that acts as a complete body scrub. It rejuvenates dull skin, removes body hair over time, and imparts a natural, festive glow.",
+        technologiesUsed: [
+            {
+                tech: "ReactJS"
+            },
+            {
+                tech: ".net"
+            },
+            {
+                tech: "REST API's"
+            },
+            {
+                tech: "Ant Design"
+            },
+            {
+                tech: "TailwindCSS"
+            },
+        ]
+    },
+    {
+        id: 0,
+        projectName: "Aloe Vera Soap",
+        image: "projects/aloeveraa.jpeg",
+        projectDetail: " Provides exceptional hydration and is perfect for soothing irritated, chapped, or sunburnt skin. It helps maintain skin suppleness and is gentle enough for sensitive skin",
+        technologiesUsed: [
+            {
+                tech: "ReactJS"
+            },
+            {
+                tech: ".net"
+            },
+            {
+                tech: "REST API's"
+            },
+            {
+                tech: "Ant Design"
+            },
+            {
+                tech: "TailwindCSS"
+            },
+        ]
+    },
+    {
+        id: 0,
+        projectName: "Sandal Soap",
+        image: "projects/sandal.jpeg",
+        projectDetail: "Renowned for its cooling effect, it soothes harsh sunburns, lightens skin pigmentation, and has a relaxing, long-lasting fragrance.",
+        technologiesUsed: [
+            {
+                tech: "ReactJS"
+            },
+            {
+                tech: ".net"
+            },
+            {
+                tech: "REST API's"
+            },
+            {
+                tech: "Ant Design"
+            },
+            {
+                tech: "TailwindCSS"
+            },
+        ]
+    },
+    
+    {
         id: 1,
-        projectName: "Otawix",
-        url: "https://b2c.otawix.com/",
-        image: "projects/otawix.png",
-        projectDetail: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam corrupti aut veritatis, adipisci natus optio dolores cum rem? Aut perferendis fugit, eos rerum totam aspernatur iure alias temporibus ipsa perspiciatis!",
+        projectName: "Pudina Soap",
+        image: "projects/pudina.jpeg",
+        projectDetail: "Provides an instant cooling sensation and is ideal for refreshing the body during hot weather while treating minor skin itchiness.",
         technologiesUsed: [
             {
                 tech: "NextJS"
@@ -187,10 +186,9 @@ const portfolio = [
     },
     {
         id: 2,
-        projectName: "Spatay",
-        url: "https://spatay.com/",
-        image: "projects/spatay.png",
-        projectDetail: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam corrupti aut veritatis, adipisci natus optio dolores cum rem? Aut perferendis fugit, eos rerum totam aspernatur iure alias temporibus ipsa perspiciatis!",
+        projectName: "Multani Mitti Soap",
+        image: "projects/multani.jpeg",
+        projectDetail: "Deeply cleanses and absorbs excess oil while tightening the skin. It helps improve skin texture by sloughing off dead cells and is great for oily or combination skin.",
         technologiesUsed: [
             {
                 tech: "ReactJS"
@@ -209,27 +207,102 @@ const portfolio = [
             },
         ]
     },
-    // {
-    //     id: 2,
-    //     projectName: "Culyte",
-    //     url: "https://culyte.com",
-    //     image: "projects/culyte.png",
-    //     projectDetail: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam corrupti aut veritatis, adipisci natus optio dolores cum rem? Aut perferendis fugit, eos rerum totam aspernatur iure alias temporibus ipsa perspiciatis!",
-    //     technologiesUsed: [
-    //         {
-    //             tech: "ReactJS"
-    //         },
-    //         {
-    //             tech: "NodeJS"
-    //         },
-    //         {
-    //             tech: "Ant Design"
-    //         },
-    //         {
-    //             tech: "TailwindCSS"
-    //         },
-    //     ]
-    // },
+    {
+        id: 2,
+        projectName: "Curry Leaves Soap",
+        image: "projects/curry leaves.jpeg",
+        projectDetail: "Though less common, curry leaves are rich in antioxidants and help in maintaining general skin health and preventing premature aging of skin cells.",
+        technologiesUsed: [
+            {
+                tech: "ReactJS"
+            },
+            {
+                tech: "NodeJS"
+            },
+            {
+                tech: "Ant Design"
+            },
+            {
+                tech: "TailwindCSS"
+            },
+        ]
+    }, {
+        id: 2,
+        projectName: "Shea Butter Soap",
+        image: "projects/shea.jpeg",
+        projectDetail: "Provides deep moisturization and forms a protective barrier to lock in hydration, making it a top choice for extremely dry or aging skin.",
+        technologiesUsed: [
+            {
+                tech: "ReactJS"
+            },
+            {
+                tech: "NodeJS"
+            },
+            {
+                tech: "Ant Design"
+            },
+            {
+                tech: "TailwindCSS"
+            },
+        ]
+    }, {
+        id: 2,
+        projectName: "Tulsi Soap",
+        image: "projects/tulasi.jpeg",
+        projectDetail: "Purifies the skin deeply, making it effective for removing impurities and preventing breakouts. It is often paired with Neem to enhance its anti-inflammatory properties.",
+        technologiesUsed: [
+            {
+                tech: "ReactJS"
+            },
+            {
+                tech: "NodeJS"
+            },
+            {
+                tech: "Ant Design"
+            },
+            {
+                tech: "TailwindCSS"
+            },
+        ]
+    },{
+        id: 2,
+        projectName: "Rose Soap",
+        image: "projects/rose.png",
+        projectDetail: "Offers a calming, aromatic experience while hydrating the skin. It helps balance the skin’s pH and is particularly beneficial for dry or sensitive skin.",
+        technologiesUsed: [
+            {
+                tech: "ReactJS"
+            },
+            {
+                tech: "NodeJS"
+            },
+            {
+                tech: "Ant Design"
+            },
+            {
+                tech: "TailwindCSS"
+            },
+        ]
+    }, {
+        id: 2,
+        projectName: "Neem Soap",
+        image: "projects/Neem.png",
+        projectDetail: "Acts as a powerful antibacterial agent, ideal for acne-prone skin. It helps combat skin infections, reduces inflammation, and regulates excess oil without stripping natural moisture.",
+        technologiesUsed: [
+            {
+                tech: "ReactJS"
+            },
+            {
+                tech: "NodeJS"
+            },
+            {
+                tech: "Ant Design"
+            },
+            {
+                tech: "TailwindCSS"
+            },
+        ]
+    }, 
 ]
 export default function handler(req, res) {
     res.status(200).json(portfolio)
