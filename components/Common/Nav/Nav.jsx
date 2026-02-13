ebef638d1fd8b5aec7b2348ea07c302f9492a707
sarthak425 <sarthak.khatpe123@gmail.com>
2026-02-13 14:02:26 +0530

    Update Nav.jsx

--- components/Common/Nav/Nav.jsx
+++ components/Common/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import { FiAward } from 'react-icons/fi'
+import { FiAward, FiBriefcase } from 'react-icons/fi'
 import { FaBars, FaHandshake } from 'react-icons/fa'
 import { ImHome } from 'react-icons/im'
 import { HiIdentification } from 'react-icons/hi'
@@ -19,7 +19,7 @@ const Nav = ({ setIsOpen, isOpen }) => {
                 </button>
                 <div className="hidden lg:flex items-center gap-x-3 text-black">
                     <NavItem setIsOpen={setIsOpen} NavRoute={'/'} NavIcon={<ImHome />} NavText={'Home'} />
-                    <NavItem setIsOpen={setIsOpen} NavRoute={'/Products'} NavIcon={<FiAward />} NavText={'Products'} />
+                    <NavItem setIsOpen={setIsOpen} NavRoute={'/portfolio'} NavIcon={<FiBriefcase />} NavText={'Products'} />
                     <NavItem setIsOpen={setIsOpen} NavRoute={'/background'} NavIcon={<HiIdentification />} NavText={'Data analytics and strategy'} />
                     <NavItem setIsOpen={setIsOpen} NavRoute={'/contact'} NavIcon={<FaHandshake />} NavText={'Contact'} />
                 </div> 
@@ -27,7 +27,7 @@ const Nav = ({ setIsOpen, isOpen }) => {
             <div className={`${isOpen ? 'block' : 'hidden'} lg:hidden border-t border-EveningBlack/60 px-4 py-3`}>
                 <div className="flex flex-col gap-y-2">
                     <NavItem setIsOpen={setIsOpen} NavRoute={'/'} NavIcon={<ImHome />} NavText={'Home'} />
-                    <NavItem setIsOpen={setIsOpen} NavRoute={'/Products'} NavIcon={<FiAward />} NavText={'Products'} />
+                    <NavItem setIsOpen={setIsOpen} NavRoute={'/portfolio'} NavIcon={<FiBriefcase />} NavText={'Products'} />
                     <NavItem setIsOpen={setIsOpen} NavRoute={'/background'} NavIcon={<HiIdentification />} NavText={'Data analytics and strategy'} />
                     <NavItem setIsOpen={setIsOpen} NavRoute={'/contact'} NavIcon={<FaHandshake />} NavText={'Contact'} />
                 </div>
